0b316d307531139f31f4a2c2965a8f02d3a375ba
xiaohuohumax <1915970986@qq.com>
2024-06-08 10:39:01 +0800

    feat: :sparkles: 添加本地搜素

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -44,6 +44,31 @@ export default defineConfig({
       quote: '这里什么都没有',
       linkLabel: '返回首页',
       linkText: '返回首页',
+    },
+    search: {
+      provider: 'local',
+      options: {
+        locales: {
+          root: {
+            translations: {
+              button: {
+                buttonText: '搜索文档',
+                buttonAriaLabel: '搜索文档'
+              },
+              modal: {
+                noResultsText: '无法找到相关结果',
+                resetButtonTitle: '清除查询条件',
+                displayDetails: '显示详细信息',
+                footer: {
+                  selectText: '选择',
+                  navigateText: '切换',
+                  closeText: '关闭'
+                }
+              }
+            }
+          }
+        }
+      }
     }
   }
 });
